8d16c0525dc1d6c9764dd78ff7a6d2aa0f5d63ba
Martin <marb61a@gmail.com>
2020-06-02 20:13:42 +0100

    Updated test base and test properties

---
+++ src/test/java/com/martin/rough/TestProperties.java
@@ -0,0 +1,16 @@
+package com.martin.rough;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Properties;
+
+public class TestProperties {
+	public static void main(String[] args) throws FileNotFoundException {
+		Properties config = new Properties();
+		FileInputStream fis = new FileInputStream("C:\\Users\\marb6\\git\\repository\\ExcelDataDrivenFramework\\src\\test\\resources\\properties\\Config.properties");
+		
+		
+	}
+	
+
+}
