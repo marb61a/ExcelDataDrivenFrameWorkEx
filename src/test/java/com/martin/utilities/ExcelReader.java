a674f131c50597c03f41a20e858e8f3be3bf2aba
Martin <marb61a@gmail.com>
2020-06-08 20:01:20 +0100

    Adding ExcelReader utility methods

--- src/test/java/com/martin/utilities/ExcelReader.java
+++ src/test/java/com/martin/utilities/ExcelReader.java
@@ -17,4 +17,35 @@ public class ExcelReader {
 	private XSSFRow row = null;
 	private XSSFCell cell = null;
 	
+	public ExcelReader(String path) {
+		this.path = path;
+		
+		try {
+			fis = new FileInputStream(path);
+			workbook = new XSSFWorkbook(fis);
+			// Get the first sheet in the workbook and close the stream when finished!!!
+			sheet = workbook.getSheetAt(0);
+			fis.close();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	// Return the row count in a sheet
+	public int getRowCount(String sheetName) {
+		int index = workbook.getSheetIndex(sheetName);
+		
+		if(index == -1) {
+			return 0;
+		} else {
+			sheet = workbook.getSheetAt(index);
+			int number = sheet.getLastRowNum();
+			return number;
+		}
+	}
+	
+	// Return the data from a cell
+	public String getCellData(String sheetName,String colName,int rowNum) {
+		
+	}
 }
