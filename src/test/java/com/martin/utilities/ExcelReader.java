0f115083840c67d1ecf82a8b60c195fdaf1b5cf3
Martin <marb61a@gmail.com>
2020-06-08 20:42:10 +0100

    Adding ExcelReader setCellData method

--- src/test/java/com/martin/utilities/ExcelReader.java
+++ src/test/java/com/martin/utilities/ExcelReader.java
@@ -2,7 +2,9 @@ package com.martin.utilities;
 
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.util.Calendar;
 
+import org.apache.poi.hssf.usermodel.HSSFDateUtil;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.xssf.usermodel.XSSFCell;
 import org.apache.poi.xssf.usermodel.XSSFRow;
@@ -85,10 +87,58 @@ public class ExcelReader {
 			} else if(cell.getCellType() == Cell.CELL_TYPE_NUMERIC || cell.getCellType()==Cell.CELL_TYPE_FORMULA) {
 				String cellText = String.valueOf(cell.getNumericCellValue());
 				
+				if(HSSFDateUtil.isCellDateFormatted(cell)){
+					double d = cell.getNumericCellValue();
+					Calendar cal =Calendar.getInstance();
+					cal.setTime (HSSFDateUtil.getJavaDate(d));
+					cellText = (String.valueOf(cal.get(Calendar.YEAR))).substring(2);
+					cellText = cal.get(Calendar.DAY_OF_MONTH) + "/" + cal.get(Calendar.MONTH)+1 + "/" + cellText;
+				}
+				
+				return cellText;
+			} else if(cell.getCellType()==Cell.CELL_TYPE_BLANK) {
+				return "";
+			} else {
+				return String.valueOf(cell.getBooleanCellValue());
 			}
 		} catch(Exception e) {
 			e.printStackTrace();
 			return "row "+rowNum+" or column "+colName +" does not exist in Excel file";
 		}
+	}
+	
+	// Will return true if data is set successfully otherwise will return false
+	public boolean setCellData(String sheetName,String colName,int rowNum, String data,String url) {
+		try {
+			fis = new FileInputStream(path);
+			workbook = new XSSFWorkbook(fis);
+			
+			// If there are no rowNum values return false
+			if(rowNum <= 0) {
+				return false;
+			}
+			
+			int index = workbook.getSheetIndex(sheetName);
+			int colNum = -1;
+			
+			if(index == -1) {
+				return false;
+			}
+			
+			sheet = workbook.getSheetAt(index);
+			row = sheet.getRow(0);
+			
+			for(int i = 0; i < row.getLastCellNum(); i++) {
+				if(row.getCell(i).getStringCellValue().trim().equalsIgnoreCase(colName)) {
+					colNum = i;
+				}
+			}
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+			return false;
+		}
+		
+		return true;
 	}
 }
