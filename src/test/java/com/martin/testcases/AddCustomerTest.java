147e28eabbe8eb2824023ef78df4409b918dcd92
Martin <marb61a@gmail.com>
2020-06-08 19:51:22 +0100

    Adding ExcelReader utility and updated Excel data file

--- src/test/java/com/martin/testcases/AddCustomerTest.java
+++ src/test/java/com/martin/testcases/AddCustomerTest.java
@@ -1,10 +1,17 @@
 package com.martin.testcases;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class AddCustomerTest {
-	@Test
-	public void addCustomer() {
+	@Test (dataProvider="getData")
+	public void addCustomer(String firstName, String lastName, String postCode) {
+		
+	}
+	
+	// Getting the data from Excel spreadsheet
+	@DataProvider
+	public Object [] [] getData() {
 		
 	}
 }
