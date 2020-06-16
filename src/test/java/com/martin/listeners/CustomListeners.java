d7fc9eaded5a89365332286910bf027d0d0c0fe0
Martin <marb61a@gmail.com>
2020-06-16 19:53:12 +0100

    Added custom listeners and updating pom

--- src/test/java/com/martin/listeners/CustomListeners.java
+++ src/test/java/com/martin/listeners/CustomListeners.java
@@ -1,7 +1,46 @@
 package com.martin.listeners;
 
+import org.testng.ITestContext;
 import org.testng.ITestListener;
+import org.testng.ITestResult;
+import org.testng.Reporter;
 
 public class CustomListeners implements ITestListener {
+
+	public void onTestStart(ITestResult result) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void onTestSuccess(ITestResult result) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void onTestFailure(ITestResult result) {
+		// Capturing screenshot on test failure
+		Reporter.log("Capturing screenshot");
+		
+	}
+
+	public void onTestSkipped(ITestResult result) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void onStart(ITestContext context) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void onFinish(ITestContext context) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
