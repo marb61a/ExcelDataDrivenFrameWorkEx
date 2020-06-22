5a500a964ce04b61fce599de1340868991184a53
Martin <marb61a@gmail.com>
2020-06-22 19:30:13 +0100

    Added ExtentManager utility for generating ExtentReports

--- src/test/java/com/martin/rough/TestTimeStamp.java
+++ src/test/java/com/martin/rough/TestTimeStamp.java
@@ -1,5 +1,12 @@
 package com.martin.rough;
 
-public class TestTimeStamp {
+import java.util.Date;
 
+public class TestTimeStamp {
+	public static void main(String[] args) {
+		Date d = new Date();
+		String screenshotName = d.toString().replace(":", "_").replace(" ", "_")+".jpg";
+		System.out.println(screenshotName);
+		System.out.println(d);
+	}
 }
